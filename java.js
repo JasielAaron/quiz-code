14404c3e8e57b7524b45bac9b268bb6ae85dadce
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2024-01-21 11:23:58 -0700

    finished my CSS HTML and started my java

---
+++ java.js
@@ -0,0 +1,39 @@
+//added questions for my quiz//
+
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+
+  {
+    title: 'The condition in an if / else state is enclosed within _____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+
+  {
+    title: 'Arrays in JavaScript can be used to stroe _____.',
+    choices: ['numbers and stings', 
+    'other arrays', 
+    'boolenas', 
+    'all of the above '],
+    answer: 'all of the above',
+  },
+
+  {
+    title: 
+    'Sting values must be enclosed within ____ when being assiged to variables',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+
+  {
+    title: 
+    'A very useful tool used during development and debugging for printiing content to debugged is:',
+    choices: ['JavaScript', 'terminal / bash ', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+
+]
